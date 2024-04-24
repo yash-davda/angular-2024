67e2f1d183d12d7d6dfd7929570ae85b9d2f7948
Yash Davda <yashdavda2001@gmail.com>
2024-04-24 19:20:17 +0530

    Interpolation

--- app/test/test.component.ts
+++ app/test/test.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-test',
   //templateUrl: './test.component.html',
   template: `<h2>This is
-                multipal line inner html</h2>`,
+                multipal line inner html</h2>
+<h1>Hello {{name}}</h1>
+<h2>{{2+2}}</h2>
+<h2>{{"Welcome " + name}}</h2>
+<h2>{{name.length}}</h2>
+<h2>{{name.toUpperCase()}}</h2>
+<h2>{{user()}}</h2>
+<h2>{{siteUrl}}</h2>`,
   /*styleUrls: ['./test.component.css']*/
   styles: [`
 h2{
@@ -15,9 +22,12 @@ h2{
 `]
 })
 export class TestComponent implements OnInit {
-
+  public name = "Yash";
+  public siteUrl = window.location.href;
   constructor() { }
-
+  user() {
+    return "Hello" + this.name;
+  }
   ngOnInit(): void {
   }
 
